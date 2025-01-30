5c435676895154faaaf157cad2a08ab008940fb1
Kaifu001 <kaifu.yang@icloud.com>
2025-01-30 17:09:48 +1300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,41 @@
+let startX, startY;
+let radius = 20;
+let intervalID;
+
 function setup() {
   createCanvas(400, 400);
+  background(180, 220, 255); 
+  startX = width / 2;
+  startY = height / 2;
+
+  
+  intervalID = setInterval(moveDot, 2000);
+
+  
+  setTimeout(stopMovingDot, 5000);
 }
 
 function draw() {
-  background(220);
+  
+  noFill();
+  stroke(0, 0, 0, 50); 
+  ellipse(startX, startY, radius * 2, radius * 2); 
+
+ 
+  let endX = random(width);
+  let endY = random(height);
+  line(startX, startY, endX, endY);
+}
+
+
+function moveDot() {
+  startX = random(width);
+  startY = random(height);
+  radius = random(15, 30); 
+}
+
+
+function stopMovingDot() {
+  clearInterval(intervalID); 
+  noLoop(); 
 }
